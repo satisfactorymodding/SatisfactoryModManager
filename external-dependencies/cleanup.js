3c09650e728e3a2733f94248717029b7abfd3f5d
mircearoata <mircearoatapalade@yahoo.ro>
2019-11-27 22:01:52 +0200

    Cache CLI download

--- external-dependencies/cleanup.js
+++
@@ -1,5 +0,0 @@
-module.exports = async function (context) {
-  const externalDependencies = require('./external-dependencies.json')
-  console.log('Cleaning up external dependencies')
-  return Promise.all(externalDependencies.map(dependency => require(dependency).cleanup(context)))
-}
--- external-dependencies/download.js
+++ external-dependencies/download.js
@@ -1,5 +1,5 @@
 module.exports = function (context) {
   const externalDependencies = require('./external-dependencies.json')
   console.log('Downloading external dependencies')
-  return Promise.all(externalDependencies.map(dependency => require(dependency).download(context)))
+  return Promise.all(externalDependencies.map(dependency => require(dependency)(context)))
 }
