01edffffd81d8c9d733adda05db877ac6b1674f5
ronifernando <ronifernando.id@gmail.com>
2020-04-23 02:05:52 +0700

    Implement plugins in renderer main js

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,6 +5,9 @@ import BootstrapVue from 'bootstrap-vue';
 import vueElectron from 'vue-electron';
 import App from './App';
 import router from './router';
+import vuetify from './plugins/vuetify';
+import EventBus from './plugins/eventBus';
+import store from './plugins/store';
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -17,10 +20,13 @@ if (!process.env.IS_WEB) Vue.use(vueElectron);
 Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+Vue.prototype.$eventBus = EventBus;
 
 /* eslint-disable no-new */
 new Vue({
   components: { App },
+  vuetify,
+  store,
   router,
   template: '<App/>',
 }).$mount('#app');
