c4d2161756afb38cadd65dc07d1a1ea479a2a6de
Chris Angelico <rosuav@gmail.com>
2021-06-30 09:15:40 +1000

    Listen only on localhost for connections from ficsit.app

--- src/main/index.js
+++ src/main/index.js
@@ -2,6 +2,7 @@ import {
   app, BrowserWindow, ipcMain, shell, screen, session,
 } from 'electron';
 import path from 'path';
+import http from 'http';
 import { autoUpdater } from 'electron-updater';
 import socketio from 'socket.io';
 import { getSetting, saveSetting } from '../settings';
@@ -287,7 +288,9 @@ if (app.requestSingleInstanceLock()) {
     });
   });
 
-  const wss = new socketio.Server(33642, { path: '/' });
+  const srv = http.createServer();
+  srv.listen(33642, '127.0.0.1');
+  const wss = new socketio.Server(srv, { path: '/' });
   wss.on('connection', (socket) => {
     socket.on('installedMods', () => {
       ipcMain.once('installedMods', (event, installedMods) => {
