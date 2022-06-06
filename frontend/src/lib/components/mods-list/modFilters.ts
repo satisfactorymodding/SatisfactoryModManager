8bce336a0be9e514c167a90d6c3e34fb17fd8d62
mircearoata <mircearoatapalade@gmail.com>
2022-06-06 15:34:53 +0300

    Add missing mod sorting options

--- frontend/src/lib/components/mods-list/modFilters.ts
+++ frontend/src/lib/components/mods-list/modFilters.ts
@@ -15,6 +15,10 @@ export interface Filter {
 export const orderByOptions: OrderBy[] = [
   { name: 'Name', func: (mod: PartialMod) => mod.name.trim() },
   { name: 'Last updated', func: (mod: PartialMod) => Date.now() - Date.parse(mod.last_version_date) },
+  { name: 'Popularity', func: (mod: PartialMod) => -mod.popularity },
+  { name: 'Hotness', func: (mod: PartialMod) => -mod.hotness },
+  { name: 'Views', func: (mod: PartialMod) => -mod.views },
+  { name: 'Downloads', func: (mod: PartialMod) => -mod.downloads },
 ];
 
 export const filterOptions: Filter[] = [
@@ -22,7 +26,7 @@ export const filterOptions: Filter[] = [
   { name: 'Installed', func: (mod: PartialMod) => mod.mod_reference in get(manifestMods) },
   { name: 'Not installed', func: (mod: PartialMod) => !(mod.mod_reference in get(manifestMods)) },
   { name: 'Enabled', func: (mod: PartialMod) => mod.mod_reference in get(lockfileMods) },
-  { name: 'Disabled', func: (mod: PartialMod) => !(mod.mod_reference in get(lockfileMods)) },
+  { name: 'Disabled', func: (mod: PartialMod) => mod.mod_reference in get(manifestMods) && !(mod.mod_reference in get(lockfileMods)) },
 ];
 
 export type PartialMod = Pick<Mod, 'id' | 'mod_reference' | 'name' | 'logo' | 'short_description' | 'views' | 'downloads' | 'last_version_date'> & { authors: { user: Pick<User, 'username'> }[] };
