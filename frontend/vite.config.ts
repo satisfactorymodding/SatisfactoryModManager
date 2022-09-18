9c7cc09e6f0f96798cf7541ce3403de41ffb22a7
mircearoata <mircearoatapalade@gmail.com>
2022-09-18 21:22:31 +0200

    Enable hmr

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte({hot: true})],
   optimizeDeps: {
     exclude: ['@urql/svelte'],
   },
