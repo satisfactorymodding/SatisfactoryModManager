f6eda166a7d02f533adbf24ee05ad94922741523
mircearoata <mircearoatapalade@gmail.com>
2022-11-13 21:32:45 +0100

    Add legendary/heroic support on windows

--- install_finders/findinstalls_windows.go
+++ install_finders/findinstalls_windows.go
@@ -12,5 +12,37 @@ func FindInstallations() ([]*Installation, []error) {
 	installs = append(installs, steamInstalls...)
 	findErrors = append(findErrors, steamFindErrors...)
 
+	heroicInstalls, heroicFindErrors := FindInstallationsWindowsHeroic()
+	for _, install := range heroicInstalls {
+		// Check if the install is already an Epic install
+		found := false
+		for _, existingInstall := range installs {
+			if existingInstall.Path == install.Path {
+				found = true
+				break
+			}
+		}
+		if !found {
+			installs = append(installs, install)
+		}
+	}
+	findErrors = append(findErrors, heroicFindErrors...)
+
+	legendaryInstalls, legendaryFindErrors := FindInstallationsWindowsLegendary()
+	for _, install := range legendaryInstalls {
+		// Check if the install is already an Epic or Heroic install
+		found := false
+		for _, existingInstall := range installs {
+			if existingInstall.Path == install.Path {
+				found = true
+				break
+			}
+		}
+		if !found {
+			installs = append(installs, install)
+		}
+	}
+	findErrors = append(findErrors, legendaryFindErrors...)
+
 	return installs, findErrors
 }
