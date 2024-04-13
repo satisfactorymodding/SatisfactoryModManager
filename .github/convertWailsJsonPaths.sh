5017455f357a4050c7151695130b03a062613e2c
mircearoata <mircearoatapalade@gmail.com>
2024-04-13 20:29:32 +0200

    Fix smmprofile icon location

--- .github/convertWailsJsonPaths.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Wails uses does not convert slashes to backslashes in fileAssociations.iconName, as it does everywhere else
-# So we keep the backslashes in the JSON for development and convert them to slashes here
-# Writing to a temporary file and then moving it is necessary because jq does not support in-place editing
-jq '.info.fileAssociations[].iconName |= gsub("\\\\"; "/")' wails.json > wails.json.tmp && mv wails.json.tmp wails.json
