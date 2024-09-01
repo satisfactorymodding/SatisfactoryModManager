88b904a7b19834443f0b9b1e0782ceba193ea794
Rob B <computerguy440+gh@gmail.com>
2024-09-01 17:05:23 -0400

    Require the user to generate debug before opening the discord. Catch situations where generating debug logs fails.

--- frontend/src/lib/components/modals/ErrorModal.svelte
+++ frontend/src/lib/components/modals/ErrorModal.svelte
@@ -1,13 +1,45 @@
 <script lang="ts">
-  import T, { translationElementPart } from '$lib/components/T.svelte';
-  import { GenerateDebugInfo } from '$wailsjs/go/app/app';
+  import T from "$lib/components/T.svelte";
+  import { BrowserOpenURL } from "$lib/generated/wailsjs/runtime/runtime";
+  import { GenerateDebugInfo } from "$wailsjs/go/app/app";
+  import { getTranslate } from "@tolgee/svelte";
 
   export let parent: { onClose: () => void };
+
+  let openDiscordTooltipText: string;
+  const { t } = getTranslate();
+  t.subscribe((getTranslationText) => {
+    openDiscordTooltipText = getTranslationText(
+      "error.open_modding_discord.must_generate_debug_first",
+      "You must generate debug info first",
+    );
+  });
+
+  let allowOpeningDiscord: boolean = false;
+
+  let onClickGenerateDebugInfo = async () => {
+    let didUserSaveFile = await GenerateDebugInfo();
+    if (didUserSaveFile) {
+      allowOpeningDiscord = true;
+      openDiscordTooltipText = "";
+    } else {
+      alert(
+        "Failed to generate and save the debug info file. Did you click the Cancel button? If not, manually check your Satisfactory Mod Manager log files for more information: https://docs.ficsit.app/satisfactory-modding/latest/faq.html#Files_Logs",
+      );
+    }
+  };
+
+  let OpenDiscord = () => {
+    BrowserOpenURL("https://discord.gg/xkVJ73E");
+  };
 
   export let error: string;
 </script>
 
-<div style="max-height: calc(100vh - 3rem); max-width: calc(100vw - 3rem);" class="w-[48rem] card flex flex-col gap-6">
+<div
+  style="max-height: calc(100vh - 3rem); max-width: calc(100vw - 3rem);"
+  class="w-[48rem] card flex flex-col gap-6"
+>
   <header class="card-header font-bold text-2xl text-center">
     <T defaultValue="Something went wrong" keyName="error.title" />
   </header>
@@ -16,25 +48,34 @@
   </section>
   <section class="px-4">
     <T
-      defaultValue="Seems wrong? Click the button below and send the generated zip file on the <1>modding discord</1> in #help-using-mods."
-      keyName="error.help"
-      parts={[
-        translationElementPart('a', {
-          href: 'https://discord.gg/xkVJ73E',
-          class: 'text-primary-600 underline',
-        }),
-      ]}
+      defaultValue="Seems wrong? Click the button below to gather logs, then send the generated zip file on the modding discord in #help-using-mods."
+      keyName="error.reporting_directions"
     />
   </section>
-  <footer class="card-footer">
+  <section class="px-4">
     <button
       class="btn text-primary-600 variant-ringed"
-      on:click={GenerateDebugInfo}>
-      <T defaultValue="Generate debug info" keyName="error.generate_debug_info" />
+      on:click={onClickGenerateDebugInfo}
+    >
+      <T
+        defaultValue="Generate debug info"
+        keyName="error.generate_debug_info"
+      />
     </button>
     <button
-      class="btn"
-      on:click={parent.onClose}>
+      class="btn text-primary-600 variant-ringed"
+      on:click={OpenDiscord}
+      disabled={!allowOpeningDiscord}
+      title={openDiscordTooltipText}
+    >
+      <T
+        defaultValue="Open the Modding Discord"
+        keyName="error.open_modding_discord"
+      />
+    </button>
+  </section>
+  <footer class="card-footer">
+    <button class="btn" on:click={parent.onClose}>
       <T defaultValue="Close" keyName="common.close" />
     </button>
   </footer>
