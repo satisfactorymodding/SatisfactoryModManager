c22a0be3c61da0f3e559e30fe35bae1b2d9771e3
Rob B <computerguy440+gh@gmail.com>
2024-09-03 16:58:12 -0400

    Simplify language selector implementation
    (cherry picked from commit a941255a58efdadccfa926a4cd898658814a00df)

--- frontend/src/lib/components/modals/first-time-setup/LanguageSelector.svelte
+++ frontend/src/lib/components/modals/first-time-setup/LanguageSelector.svelte
@@ -5,51 +5,31 @@
 
   let languages: string[] = Object.keys(i18n);
 
-  type DropdownItem = {
-    value: string;
-    label: string;
-  };
-
-  function prepLocale(locale: string) {
-    return {
-      value: locale,
-      label: localeName(locale),
-    };
-  }
-
-  let dropdownItemData = languages.map(prepLocale);
-  
   function localeName(locale: string) {
     if (!locale) return 'N/A';
     return new Intl.DisplayNames([locale], { type: 'language' }).of(locale) ?? `Error:${locale}`;
   }
-
-  async function languageSelectChanged(selection: CustomEvent<DropdownItem>) {
-    $language = selection.detail.value;
-  }
 </script>
 
-<!-- TODO the formatting on this is totally botched, isn't going vertical, is starting open? -->
 <Select
   name="languageCombobox"
-  class="w-full h-15"
+  class="w-full"
   buttonClass="bg-surface-200-700-token px-4 text-sm"
   itemActiveClass="!bg-surface-300/20"
   itemClass="bg-surface-50-900-token"
-  items={dropdownItemData}
+  items={languages}
   menuClass="bg-surface-50-900-token"
 
-  value={prepLocale($language) ?? 'UNSET'}
-  on:change={languageSelectChanged}
+  bind:value={$language}
 >
   <svelte:fragment slot="item" let:item>
-    <span>{item.label}</span>
+    <span>{localeName(item)}</span>
   </svelte:fragment>
   <!-- TODO: dynamic flags 
-    <svelte:fragment slot="itemTrail" let:item>
-      <span slot="lead" class="h-5 w-5 block">
-        {localeFlag(item.value)}
-      </span>
-    </svelte:fragment>
-    -->
+  <svelte:fragment slot="itemTrail" let:item>
+    <span slot="lead" class="h-5 w-5 block">
+      {localeFlag(item.value)}
+    </span>
+  </svelte:fragment>
+  -->
 </Select>
