f0fc116b3cefd58dd4058c7b8d3cf113faf83b3a
mircearoata <mircearoatapalade@gmail.com>
2024-09-10 15:37:35 +0200

    Update ficsit-cli cache

--- backend/ficsitcli/offline.go
+++ backend/ficsitcli/offline.go
@@ -1,12 +1,15 @@
 package ficsitcli
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
 	"slices"
 	"strings"
 
 	ficsitcache "github.com/satisfactorymodding/ficsit-cli/cli/cache"
 	"github.com/satisfactorymodding/ficsit-cli/cli/provider"
+	resolver "github.com/satisfactorymodding/ficsit-resolver"
 
 	"github.com/satisfactorymodding/SatisfactoryModManager/backend/settings"
 )
@@ -22,103 +25,73 @@ func (f *ficsitCLI) SetOffline(offline bool) {
 }
 
 type Mod struct {
-	ModReference string       `json:"mod_reference"`
-	Name         string       `json:"name"`
-	Logo         *string      `json:"logo"` // Base64 encoded
-	Authors      []string     `json:"authors"`
-	Versions     []ModVersion `json:"versions"`
-}
-
-type ModVersion struct {
-	Version      string                 `json:"version"`
-	GameVersion  string                 `json:"game_version"`
-	Size         int64                  `json:"size"`
-	Dependencies []ModVersionDependency `json:"dependencies"`
-}
-
-type ModVersionDependency struct {
-	ModReference string `json:"mod_id"`
-	Condition    string `json:"condition"`
-	Optional     bool   `json:"optional"`
+	ModReference string                `json:"mod_reference"`
+	Name         string                `json:"name"`
+	Logo         *string               `json:"logo"` // Base64 encoded
+	Authors      []string              `json:"authors"`
+	Versions     []resolver.ModVersion `json:"versions"`
 }
 
 func (f *ficsitCLI) OfflineGetMods() ([]Mod, error) {
-	cache, err := ficsitcache.GetCache()
+	cache, err := ficsitcache.GetCacheMods()
 	if err != nil {
 		return nil, fmt.Errorf("failed to get cache: %w", err)
 	}
 
 	mods := make([]Mod, 0)
-	cache.Range(func(modReference string, modFiles []ficsitcache.File) bool {
-		mods = append(mods, convertCacheFileToMod(modFiles))
+	cache.Range(func(modReference string, mod ficsitcache.Mod) bool {
+		mods = append(mods, f.convertCacheFileToMod(mod))
 		return true
 	})
 	return mods, nil
 }
 
 func (f *ficsitCLI) OfflineGetModsByReferences(modReferences []string) ([]Mod, error) {
-	cache, err := ficsitcache.GetCache()
+	cache, err := ficsitcache.GetCacheMods()
 	if err != nil {
 		return nil, fmt.Errorf("failed to get cache: %w", err)
 	}
 
 	mods := make([]Mod, 0)
-	cache.Range(func(modReference string, modFiles []ficsitcache.File) bool {
+	cache.Range(func(modReference string, mod ficsitcache.Mod) bool {
 		if !slices.Contains(modReferences, modReference) {
 			return true
 		}
-		mods = append(mods, convertCacheFileToMod(modFiles))
+		mods = append(mods, f.convertCacheFileToMod(mod))
 		return true
 	})
 	return mods, nil
 }
 
 func (f *ficsitCLI) OfflineGetMod(modReference string) (Mod, error) {
-	modFiles, err := ficsitcache.GetCacheMod(modReference)
+	mod, err := ficsitcache.GetCacheMod(modReference)
 	if err != nil {
 		return Mod{}, fmt.Errorf("failed to get cache: %w", err)
 	}
-	if modFiles == nil {
-		return Mod{}, fmt.Errorf("mod not found")
-	}
-	return convertCacheFileToMod(modFiles), nil
+	return f.convertCacheFileToMod(mod), nil
 }
 
-func convertCacheFileToMod(files []ficsitcache.File) Mod {
+func (f *ficsitCLI) convertCacheFileToMod(mod ficsitcache.Mod) Mod {
+	if mod.ModReference == "" {
+		return Mod{}
+	}
+
 	authors := make([]string, 0)
 
-	for _, author := range strings.Split(files[0].Plugin.CreatedBy, ",") {
+	for _, author := range strings.Split(mod.Author, ",") {
 		authors = append(authors, strings.TrimSpace(author))
 	}
 
-	versions := make([]ModVersion, 0)
-	for _, file := range files {
-		dependencies := make([]ModVersionDependency, 0)
-
-		for _, dependency := range file.Plugin.Plugins {
-			if dependency.BasePlugin {
-				continue
-			}
-			dependencies = append(dependencies, ModVersionDependency{
-				ModReference: dependency.Name,
-				Condition:    dependency.SemVersion,
-				Optional:     dependency.Optional,
-			})
-		}
-
-		versions = append(versions, ModVersion{
-			Version:      file.Plugin.SemVersion,
-			GameVersion:  file.Plugin.GameVersion,
-			Size:         file.Size,
-			Dependencies: dependencies,
-		})
+	modVersions, err := f.ficsitCli.Provider.ModVersionsWithDependencies(context.TODO(), mod.ModReference)
+	if err != nil {
+		slog.Warn("failed to get mod versions", slog.String("mod", mod.ModReference), slog.Any("error", err))
 	}
 
 	return Mod{
-		Name:         files[0].Plugin.FriendlyName,
-		ModReference: files[0].ModReference,
+		Name:         mod.Name,
+		ModReference: mod.ModReference,
 		Authors:      authors,
-		Logo:         files[0].Icon,
-		Versions:     versions,
+		Logo:         mod.Icon,
+		Versions:     modVersions,
 	}
 }
