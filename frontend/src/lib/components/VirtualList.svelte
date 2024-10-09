001a6ad878e484c27ffb895a81788dd386edb534
mircearoata <mircearoatapalade@gmail.com>
2024-10-09 12:13:42 +0200

    Fix mod list scrolling to top when clicking install button

--- frontend/src/lib/components/VirtualList.svelte
+++ frontend/src/lib/components/VirtualList.svelte
@@ -1,6 +1,5 @@
 <script generics="T" lang="ts">
   import { createVirtualizer } from '@tanstack/svelte-virtual';
-  import _ from 'lodash';
 
   // eslint-disable-next-line no-undef
   export let items: T[];
@@ -9,21 +8,46 @@
   let clazz = '';
   export { clazz as class };
   export let itemClass = '';
-  
+
   let virtualListEl: HTMLDivElement;
 
-  $: virtualizer = createVirtualizer<HTMLDivElement, HTMLDivElement>({
+  let virtualizer = createVirtualizer<HTMLDivElement, HTMLDivElement>({
     count: items.length,
     getScrollElement: () => virtualListEl,
     estimateSize: () => itemHeight ?? 100,
     overscan,
   });
 
-  $: vitems = $virtualizer.getVirtualItems();
-  
+  let virtualizerUpdateWrapper = virtualizer;
+
+  function updateVirtualizer() {
+    $virtualizer.setOptions({
+      count: items.length,
+      getScrollElement: () => virtualListEl,
+      estimateSize: () => itemHeight ?? 100,
+      overscan,
+    });
+    virtualizerUpdateWrapper = virtualizer;
+  }
+
+  $: {
+    // Don't subscribe to the store here, so we don't call setOptions on scroll (the store pushes a new value on scroll)
+    items;
+    itemHeight;
+    overscan;
+    virtualListEl;
+    updateVirtualizer();
+  }
+
+  $: vitems = $virtualizerUpdateWrapper.getVirtualItems();
+
+  function measure(el: HTMLDivElement) {
+    $virtualizerUpdateWrapper.measureElement(el);
+  }
+
   let virtualItemEls: HTMLDivElement[] = [];
   $: if (virtualItemEls.length) {
-    virtualItemEls.forEach((el) => $virtualizer.measureElement(el));
+    virtualItemEls.forEach(measure);
   }
 </script>
 
@@ -33,12 +57,12 @@
   class="relative overflow-y-scroll h-full {clazz}"
 >
   <div
-    style="height: {$virtualizer.getTotalSize()}px; width: 100%;"
+    style="height: {$virtualizerUpdateWrapper.getTotalSize()}px; width: 100%;"
     class="overflow-hidden"
   >
     <div
       style="transform: translateY({vitems[0]
-        ? vitems[0].start - $virtualizer.options.scrollMargin
+        ? vitems[0].start - $virtualizerUpdateWrapper.options.scrollMargin
         : 0}px);"
       class="absolute top-0 left-0 w-full overflow-hidden"
     >
