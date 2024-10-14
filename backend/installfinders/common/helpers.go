eb2bd6738f8a3af811704cfd3e10d4ac9d0298b5
mircearoata <mircearoatapalade@gmail.com>
2024-10-14 15:33:23 +0200

    Fix steam folder symlink causing duplicate installs on linux

--- backend/installfinders/common/helpers.go
+++ backend/installfinders/common/helpers.go
@@ -1,18 +1,28 @@
 package common
 
 import (
+	"log/slog"
 	"path/filepath"
 	"runtime"
 	"strings"
 )
 
 func OsPathEqual(path1, path2 string) bool {
-	path1 = filepath.Clean(path1)
-	path2 = filepath.Clean(path2)
+	path1 = realPath(path1)
+	path2 = realPath(path2)
 	if runtime.GOOS == "windows" {
 		return strings.EqualFold(path1, path2)
 	}
 	return path1 == path2
+}
+
+func realPath(path string) string {
+	newPath, err := filepath.EvalSymlinks(path)
+	if err != nil {
+		slog.Warn("failed to evaluate symlink, using original path", slog.String("path", path), slog.Any("error", err))
+		return path
+	}
+	return newPath
 }
 
 func FindAll(finders ...InstallFinderFunc) ([]*Installation, []error) {
