e25576fc431afaa7e337a9bc6e11e4cb3d328c30
Rob B <computerguy440+gh@gmail.com>
2024-12-31 17:48:11 -0600

    Update "installed as a dependency" help text
    Already updated on Tolgee
    re: https://discord.com/channels/555424930502541343/1036634533077979146/1323553969557209160

--- frontend/src/lib/components/mods-list/ModsListItem.svelte
+++ frontend/src/lib/components/mods-list/ModsListItem.svelte
@@ -85,7 +85,7 @@
       return {
         icon: mdiLinkLock,
         iconHover: mdiLinkLock,
-        tooltip: $t('mod-list-item.dependency', 'This mod is installed as a dependency of another mod. It cannot be installed or removed on its own.'),
+        tooltip: $t('mod-list-item.dependency', 'This mod is already installed as a dependency of another mod. It cannot be (un)installed or removed on its own.'),
       };
     }
     if (queuedInstall) {
